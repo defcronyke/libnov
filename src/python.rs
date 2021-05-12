f22de0128fcddc5ca0d93bfc956110f5c52399d6
Jeremy Carter <jeremy@jeremycarter.ca>
2021-05-12 14:37:28 -0400

    Fix CI/CD.

--- src/python.rs
+++ src/python.rs
@@ -15,30 +15,53 @@
     this project's license terms taking first priority.
 */
 
-use cpython::{PyDict, PyResult, Python};
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
 
-pub fn init() {
-    let gil = Python::acquire_gil();
-    hello(gil.python()).unwrap();
+pub fn init() -> Result<(), ()> {
+    Python::with_gil(|py| {
+        main_(py).map_err(|e| {
+            // We can't display Python exceptions via std::fmt::Display,
+            // so print the error here manually.
+            e.print_and_set_sys_last_vars(py);
+        })
+    })
 }
 
-fn hello(py: Python) -> PyResult<()> {
+fn main_(py: Python) -> PyResult<()> {
     let sys = py.import("sys")?;
-    let version: String = sys.get(py, "version")?.extract(py)?;
-
-    let locals = PyDict::new(py);
-    locals.set_item(py, "os", py.import("os")?)?;
-    let user: String = py
-        .eval(
-            "os.getenv('USER') or os.getenv('USERNAME')",
-            None,
-            Some(&locals),
-        )?
-        .extract(py)?;
-
+    let version: String = sys.get("version")?.extract()?;
+    let locals = [("os", py.import("os")?)].into_py_dict(py);
+    let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+    let user: String = py.eval(code, None, Some(&locals))?.extract()?;
     println!("Hello {}, I'm Python {}", user, version);
     Ok(())
 }
+
+// use cpython::{PyDict, PyResult, Python};
+
+// pub fn init() {
+//     let gil = Python::acquire_gil();
+//     hello(gil.python()).unwrap();
+// }
+
+// fn hello(py: Python) -> PyResult<()> {
+//     let sys = py.import("sys")?;
+//     let version: String = sys.get(py, "version")?.extract(py)?;
+
+//     let locals = PyDict::new(py);
+//     locals.set_item(py, "os", py.import("os")?)?;
+//     let user: String = py
+//         .eval(
+//             "os.getenv('USER') or os.getenv('USERNAME')",
+//             None,
+//             Some(&locals),
+//         )?
+//         .extract(py)?;
+
+//     println!("Hello {}, I'm Python {}", user, version);
+//     Ok(())
+// }
 
 // use pyembed::*;
 
